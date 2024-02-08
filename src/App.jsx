6dc190a6b9e3dff978db29fe3a73fca81460990c
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-08 21:02:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,17 @@ import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 import SidebarIcons from "./components/SidebarIcons";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 
 function App() {
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const isDesktop = useMediaQuery({ minWidth: 1250 });
+  // const isDesktop = useMediaQuery({ minWidth: 1250 });
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
       <SContainer>
-        {isDesktop && <SidebarIcons />}
+        <SidebarIcons />
+        {/* {isDesktop && <SidebarIcons />} */}
         <Header toggleTheme={toggleTheme} theme={theme} />
         <Banner />
         <Footer />
